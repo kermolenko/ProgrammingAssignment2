0af1c57912a50d9d6692a5667af4c7c1a56c0df1
kermolenko <kermolenko@users.noreply.github.com>
2015-05-24 22:14:38 +0300

    Program assignment 2
    Matrix cache function

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,19 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        ### define cache object and required function
+        s <- NULL
+        set <- function(y) {
+                x <<- y
+                s <<- NULL
+        }
+        get <- function() x
+        setsolve <- function(solve) m <<- solve
+        getsolve <- function() s
+        ## map and return adjusted matrix
+        list(set = set, get = get,
+             setsolve = setsolve,
+             getsolve = getsolve)
 }
 
 
@@ -12,4 +24,15 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        s <- x$getsolve()
+        ## return cached value
+        if(!is.null(s)) {
+                message("getting cached data")
+                return(s)
+        }
+        ## calculate value and cache it
+        data <- x$get()
+        s <- solve(data, ...)
+        x$setsolve(s)
+        s
 }
